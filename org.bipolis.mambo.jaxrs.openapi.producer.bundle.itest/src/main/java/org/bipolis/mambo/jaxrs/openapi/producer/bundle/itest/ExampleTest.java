f272f92a22052fa99c4899814afbb1d224f5b0e3
stbischof <stbischof@bipolis.org>
2018-05-01 14:12:47 +0200

    ci

--- org.bipolis.mambo.jaxrs.openapi.producer.bundle.itest/src/main/java/org/bipolis/mambo/jaxrs/openapi/producer/bundle/itest/ExampleTest.java
+++ org.bipolis.mambo.jaxrs.openapi.producer.bundle.itest/src/main/java/org/bipolis/mambo/jaxrs/openapi/producer/bundle/itest/ExampleTest.java
@@ -20,68 +20,62 @@ import io.swagger.v3.oas.models.info.License;
 
 public class ExampleTest {
 
-  private final BundleContext context = FrameworkUtil.getBundle(ExampleTest.class)
-                                                     .getBundleContext();
+	private final BundleContext context = FrameworkUtil.getBundle(ExampleTest.class).getBundleContext();
 
-  @BeforeClass
-  public static void beforeAll() {
-    try {
-      Thread.sleep(5000);
-    } catch (InterruptedException e) {
-      e.printStackTrace();
-    }
-  }
+	@BeforeClass
+	public static void beforeAll() {
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
 
-  @Test
-  public void testExample()
-          throws InterruptedException {
+	@Test
+	public void testExample() throws InterruptedException {
 
-    ServiceTracker<OpenApiFragmentsService, ? extends OpenApiFragmentsService> tracker =
-            new ServiceTracker<>(context, OpenApiFragmentsService.class.getName(), null);
+		ServiceTracker<OpenApiFragmentsService, ? extends OpenApiFragmentsService> tracker = new ServiceTracker<>(
+				context, OpenApiFragmentsService.class.getName(), null);
 
-    tracker.open();
-    OpenApiFragmentsService openApiFragmentsService = tracker.waitForService(5 * 1000);
+		tracker.open();
+		OpenApiFragmentsService openApiFragmentsService = tracker.waitForService(5 * 1000);
 
-    assertNotNull("Service not null", openApiFragmentsService);
+		assertNotNull("Service should not be null", openApiFragmentsService);
 
-    assertEquals("fetch correct Service", openApiFragmentsService.getClass()
-                                                                 .getName(),
-            "org.bipolis.mambo.jaxrs.openapi.producer.bundle.BundleApiAppanderService");
+		assertEquals("fetch correct Service", openApiFragmentsService.getClass().getName(),
+				"org.bipolis.mambo.jaxrs.openapi.producer.bundle.BundleApiAppanderService");
 
-    OpenAPI openAPI = openApiFragmentsService.getFragmentOpenApi(ExampleApplication.APP_NAME);
+		OpenAPI openAPI = openApiFragmentsService.getFragmentOpenApi(ExampleApplication.APP_NAME);
 
-    // this test application, default app.
-    assertNotNull("expected Applications", openAPI);
+		// this test application, default app.
+		assertNotNull("expected Applications", openAPI);
 
-    Bundle bundle = FrameworkUtil.getBundle(ExampleApplication.class);
+		Bundle bundle = FrameworkUtil.getBundle(ExampleApplication.class);
 
-    Info info = openAPI.getInfo();
+		Info info = openAPI.getInfo();
 
-    Contact contact = info.getContact();
-    License license = info.getLicense();
-    ExternalDocumentation externalDocumentation = openAPI.getExternalDocs();
+		Contact contact = info.getContact();
+		License license = info.getLicense();
+		ExternalDocumentation externalDocumentation = openAPI.getExternalDocs();
 
-    assertEquals(info.getTitle(), getBundleProp(bundle, "Bundle-Name"));
-    assertEquals(info.getDescription(), getBundleProp(bundle, "Bundle-Description"));
-    assertEquals(info.getTermsOfService(), getBundleProp(bundle, "Bundle-Copyright"));
-    assertEquals(license.getUrl(), getBundleProp(bundle, "Bundle-License"));
-    assertEquals(contact.getName(), getBundleProp(bundle, "Bundle-Vendor"));
-    assertEquals(contact.getEmail(), getBundleProp(bundle, "Bundle-ContactAddress"));
-    assertEquals(externalDocumentation.getUrl(), getBundleProp(bundle, "Bundle-DocURL"));
-    assertEquals(info.getVersion(), bundle.getVersion()
-                                          .toString());
+		assertEquals(info.getTitle(), getBundleProp(bundle, "Bundle-Name"));
+		assertEquals(info.getDescription(), getBundleProp(bundle, "Bundle-Description"));
+		assertEquals(info.getTermsOfService(), getBundleProp(bundle, "Bundle-Copyright"));
+		assertEquals(license.getUrl(), getBundleProp(bundle, "Bundle-License"));
+		assertEquals(contact.getName(), getBundleProp(bundle, "Bundle-Vendor"));
+		assertEquals(contact.getEmail(), getBundleProp(bundle, "Bundle-ContactAddress"));
+		assertEquals(externalDocumentation.getUrl(), getBundleProp(bundle, "Bundle-DocURL"));
+		assertEquals(info.getVersion(), bundle.getVersion().toString());
 
-    tracker.close();
-  }
+		tracker.close();
+	}
 
-  private static String getBundleProp(Bundle bundle,
-                                      String property) {
+	private static String getBundleProp(Bundle bundle, String property) {
 
-    String valueFromBundle = null;
-    if (bundle != null) {
-      valueFromBundle = bundle.getHeaders()
-                              .get(property);
-    }
-    return valueFromBundle;
-  }
+		String valueFromBundle = null;
+		if (bundle != null) {
+			valueFromBundle = bundle.getHeaders().get(property);
+		}
+		return valueFromBundle;
+	}
 }
